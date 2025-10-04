30fb44865be2f26774d3f5f7a13d68a2128b65b1
Yuriy Volodin <Goliakywillalive@yandex.ru>
2025-10-04 15:59:50 +0500

    Fixed tests.md

--- src/test/java/ru/netology/iqa116/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/iqa116/CashbackHackServiceTest.java
@@ -5,30 +5,33 @@ import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
-    @Test //#1 - Докинем 100 чтоб был кешбек?
+    @Test //#1 - Докупи на 100р, чтоб получить кешбек
     public void shouldReturn100IfAmountIs900() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int actual = service.remain(amount);
+
+        int actual = service.remain(900);
         int expected = 100;
+
         Assert.assertEquals(actual, expected);
     }
 
-    @Test //#2 - Нужно ли докидывать если сумма кратна той, что заявлена?
+    @Test //#2 - Нужно ли докупать, если покупка кратна 1000р?
     public void shouldReturn0IfAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 1_000;
-        int actual = service.remain(amount);
+
+        int actual = service.remain(1_000);
         int expected = 0;
+
         Assert.assertEquals(actual, expected);
     }
 
-    @Test //№3 - Почти аналогично предыдущим, но в данном случае, проверяем сколько нужно докинуть, если оплата была выше чем 1000р
+    @Test //№3 - Нужно ли докупать, если покупка более 1000р и делится с остатком
     public void shouldReturn500IfAmountIs2500() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 2_500;
-        int actual = service.remain(amount);
+
+        int actual = service.remain(2_500);
         int expected = 500;
+
         Assert.assertEquals(actual, expected);
     }
 }
